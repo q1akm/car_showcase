03f7934a7969bc86eeeca88185495292a7865bfb
Aung Kaung Myint <aungkggmyint@gmail.com>
2024-05-13 16:48:35 +0700

    starting fetch data

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -25,7 +25,7 @@ const SearchManufacturer = ({manufacturer, setManufacturer} : SearchManufacturer
 
   return (
     <div className="search-manufacturer">
-      <Combobox>
+      <Combobox value={manufacturer} onChange={setManufacturer}>
         <div className="relative w-full">
           <ComboboxButton className="absolute top-[14px]">
             <Image src="/car-logo.svg"
@@ -46,27 +46,30 @@ const SearchManufacturer = ({manufacturer, setManufacturer} : SearchManufacturer
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
             afterLeave={()=> setQuery('')}>
-              <ComboboxOptions>
-               {filteredManufacturer.length === 0 && 
-               query !== "" ? (
-                <ComboboxOption
-                value={query}
-                className="search__manufacturer__option">
-                  Create "{query}"
-                </ComboboxOption>
-               ): (
-                filteredManufacturer.map((item)=> (
+             <ComboboxOptions>
+                {filteredManufacturer.map((item)=> (
                    <ComboboxOption
                    key={item}
                    className={({ active}) => `
                    relative search-manufacturer__option 
                    ${active ? 'bg-primary-blue text-white' : 'text-gray-900'}`}
                    value={item}>
-                      {item}
+                       {({ selected, active }) => (
+                      <>
+                        <span className={`block truncate ${selected ? "font-medium" : "font-normal"}`}>
+                          {item}
+                        </span>
+
+                      
+                        {selected ? (
+                          <span className={`absolute inset-y-0 left-0 flex items-center pl-3 ${active? "text-white": "text-pribg-primary-purple"}`}
+                          ></span>
+                        ) : null}
+                      </>
+                    )}
                    </ComboboxOption> 
-                ))
-               )}
-              </ComboboxOptions>
+                ))}
+               </ComboboxOptions> 
           </Transition>
         </div>
       </Combobox>
