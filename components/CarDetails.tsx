5c95438687afa10d58e9d97c1634165156fdf1d2
Aung Kaung Myint <aungkggmyint@gmail.com>
2024-05-16 17:42:55 +0700

    some changes

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import { Fragment } from "react";
 import { CarProps } from "@/types"
 
-import { Dialog,Transition, TransitionChild } from "@headlessui/react";
+import { Dialog,DialogPanel,Transition, TransitionChild } from "@headlessui/react";
 
 interface CarDetailsProps {
 
@@ -19,7 +19,9 @@ const CarDetails = ({isOpen, closeModal, car}: CarDetailsProps) => {
     <>
       <Transition appear show={isOpen} as={Fragment}>
         <Dialog as="div" className="relative z-10" onClose={closeModal}>
-          <TransitionChild as={Fragment} enter="ease-out duration-300"
+          <TransitionChild 
+          as={Fragment} 
+          enter="ease-out duration-300"
           enterFrom="opacity-0"
           enterTo="opacity-100"
           leave="ease-in duration-200"
@@ -30,8 +32,30 @@ const CarDetails = ({isOpen, closeModal, car}: CarDetailsProps) => {
 
           </TransitionChild>
 
-          <div className="fixed inset-0 overflow-y-0">
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <TransitionChild
+               as={Fragment} 
+               enter="ease-out duration-300"
+               enterFrom="opacity-0 scale-95"
+               enterTo="opacity-100 scale-100"
+               leave="ease-in duration-200"
+               leaveFrom="opacity-100 scale-100"
+               leaveTo="opacity-0 scale-95">
+                <DialogPanel>
+                  <button 
+                  type="button"
+                  onClick={closeModal}>
+                    <Image src="/close.svg"
+                    alt="close"
+                    width={20}
+                    height={20}
+                    className="object-contain"/>
+                  </button>
+                </DialogPanel>
+              </TransitionChild>
 
+            </div>
           </div>
 
         </Dialog>
