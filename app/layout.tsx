59cbdc276724072642379866b375e6ca43432966
Aung Kaung Myint <aungkggmyint@gmail.com>
2024-09-07 16:06:17 +0700

    change title

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 import { Footer, NavBar } from "@/components";
 
 export const metadata = {
-  title: "Car Hub",
+  title: "Car Showcase",
   description: "Discover world's best car showcase application",
 };
 
